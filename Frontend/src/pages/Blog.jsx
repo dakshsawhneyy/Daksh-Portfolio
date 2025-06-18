95156b719d94a2593be9d91064f4790bb4698715
Daksh Sawhney <162882890+dakshsawhneyy@users.noreply.github.com>
2025-06-19 03:49:24 +0530

    Update Blog.jsx

--- Frontend/src/pages/Blog.jsx
+++ Frontend/src/pages/Blog.jsx
@@ -30,7 +30,7 @@ const Blog = () => {
         {blogs.map((item, index) => (
           <Tilt key={index} glareEnable glareMaxOpacity={0.2} scale={1.05} transitionSpeed={400} className="bg-white dark:bg-[#0f0f0f] relative overflow-hidden rounded-2xl shadow-2xl backdrop-blur-lg border border-green-700/50">
             <motion.div key={index} initial={{ opacity: 0, y: 20 }} whileInView={{ opacity: 1, y: 0 }} transition={{ delay: index * 0.1 }} className=" p-4 rounded-lg hover:shadow-green-400/30 transition-all cursor-pointer " >
-              <img src={item.coverImage?.url} alt={item.title} className="w-full h-40 object-cover mb-4 border border-[#6366F1] dark:border-green-700/40 rounded" />
+              <img src={item.coverImage?.url} alt={item.title} className="w-full h-40 object-cover mb-4 dark:border dark:border-green-700/40 rounded" />
               <h3 className="text-[#0F172A] dark:text-success text-lg font-semibold mb-2 terminal-glow">{item.title}</h3>
               <p className="text-[#475569] dark:text-green-300 text-sm">{item.brief.slice(0, 140)}...</p>
               <a href={`https://hashnode.com/post/${item.slug}`} target="_blank" rel="noreferrer" className="block mt-3 text-[#6366F1] dark:text-accent dark:hover:text-green-100 hover:text-black transition" >
